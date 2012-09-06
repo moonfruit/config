e0392b0e4870776c091cf8bc2e4a71f71b562fb4
MoonFruit <dkmoonfruit@gmail.com>
2012-09-06 09:47:22 +0800

    bug fix

--- zsh/dir.zsh
+++ zsh/dir.zsh
@@ -12,5 +12,20 @@ hash -d CONFIG="$CONFIG"
 export  APP="$WORKSPACE/app"
 hash -d APP="$APP"
 
+export  DEV="$WORKSPACE/app"
+hash -d DEV="$DEV"
+
 export  UFRAME="$APP/prods/uframe"
-hash -d UFRAME="$UFAME"
+hash -d UFRAME="$UFRAME"
+
+export  UPLATFORM="$APP/prods/uplatform"
+hash -d UPLATFORM="$UPLATFORM"
+
+export  UTELLER="$APP/prods/uteller"
+hash -d UTELLER="$UTELLER"
+
+export  CNAPS2="$APP/prods/cnaps2"
+hash -d CNAPS2="$CNAPS2"
+
+export  YY="$APP/prods/yy"
+hash -d YY="$YY"
